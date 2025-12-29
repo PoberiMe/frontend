812426bdff405ff2fc1f290983f6dbd66522ca3f
Matjaž Auberšek <ma4481@student.uni-lj.si>
2025-12-29 01:07:40 +0100

    session

--- src/app/core/services/auth.service.ts
+++ src/app/core/services/auth.service.ts
@@ -1,6 +1,15 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import {jwtDecode} from 'jwt-decode';
+
+interface JwtPayload {
+  sub: string; // userId
+  username: string;
+  iat: number;
+  exp: number;
+}
+
 
 @Injectable({
   providedIn: 'root'
@@ -22,5 +31,29 @@ export class AuthService {
     return this.http.post(`${this.apiUrl}/login`, { identifier, password }, {
       responseType: 'text'
     });
+  }
+
+  getCurrentUser(): JwtPayload | null {
+    const token = localStorage.getItem('jwt');
+    if (!token) return null;
+
+    return jwtDecode<JwtPayload>(token);
+  }
+
+  getUserId(): string | null {
+    const user = this.getCurrentUser();
+    return user?.sub || null;
+  }
+
+  isLoggedIn(): boolean {
+    const token = localStorage.getItem('jwt');
+    if (!token) return false;
+
+    const decoded = jwtDecode<JwtPayload>(token);
+    return decoded.exp * 1000 > Date.now();
+  }
+
+  logout(): void {
+    localStorage.removeItem('jwt');
   }
 }
--- src/app/layout/header/header.ts
+++ src/app/layout/header/header.ts
@@ -1,14 +1,15 @@
 import { Component } from '@angular/core';
-import {RouterLink} from '@angular/router';
+import { RouterLink } from '@angular/router';
+import { AuthService } from '../../core/services/auth.service';
 
 @Component({
   selector: 'app-header',
-  imports: [
-    RouterLink
-  ],
+  imports: [RouterLink],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
 export class Header {
   mode: 'passenger' | 'driver' = 'passenger';
+
+  constructor(public authService: AuthService) {}
 }
